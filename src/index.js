13b9622c5339773a1c1cee3b4a9ddcacb7aa0e1f
Philipp Bous <wolf1bous@googlemail.com>
2021-09-15 16:45:07 +0200

    ADDS very basic webpack setup

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { Chart } from "chart.js";
+import './scss/style.scss';
+
+function go() {
+    new Chart(
+        document.getElementById('myChart'),
+        config
+    );
+}
+
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+go();
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin({title: "Output Management", template: "index.html"})],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      }
+    ]
+  }
+};
