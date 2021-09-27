8ca8f3163f2b9744718225188d5fa6875ee6b3ba
Philipp Bous <wolf1bous@googlemail.com>
2021-09-27 09:56:14 +0200

    ADDS basic markup + chart-example

--- src/index.js
+++ src/index.js
@@ -1,36 +1,52 @@
-import { Chart } from "chart.js";
+import { Chart, RadarController, LineElement, PointElement, Title, RadialLinearScale } from 'chart.js';
+
 import './scss/style.scss';
-
-function go() {
-    new Chart(
-        document.getElementById('myChart'),
-        config
-    );
-}
-
-const labels = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-];
+import $ from "jquery";
 
 const data = {
-  labels: labels,
+  labels: [
+    'Eating',
+    'Drinking',
+    'Sleeping',
+    'Designing',
+    'Coding',
+  ],
   datasets: [{
-    label: 'My First dataset',
-    backgroundColor: 'rgb(255, 99, 132)',
+    label: 'My First Dataset',
+    data: [6, 5, 9, 8, 5],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
     borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30, 45],
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }, {
+    label: 'My Second Dataset',
+    data: [2, 4, 4, 1, 9],
+    fill: true,
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    borderColor: 'rgb(54, 162, 235)',
+    pointBackgroundColor: 'rgb(54, 162, 235)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(54, 162, 235)'
   }]
 };
 
 const config = {
-    type: 'line',
-    data: data,
-    options: {}
+  type: "radar",
+  data: data,
+  options: {
+    elements: {
+      line: {
+        borderWidth: 3
+      }
+    }
+  }
 };
 
-go();
+$(function () {
+    Chart.register(RadarController, LineElement, PointElement, RadialLinearScale, Title);
+    new Chart($(".character-mainStats--radar-chart").get(), config);
+});
