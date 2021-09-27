bd7095631af9aecd606f8f2ba24a1712ca2ef99e
Philipp Bous <wolf1bous@googlemail.com>
2021-09-27 10:37:38 +0200

    ADDS Paladin base-stats

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import { Chart, RadarController, LineElement, PointElement, Title, RadialLinearS
 
 import './scss/style.scss';
 import $ from "jquery";
+import Paladin from "./paladin.svg";
 
 const data = {
   labels: [
-    'Eating',
-    'Drinking',
-    'Sleeping',
-    'Designing',
-    'Coding',
+    'Stärke',
+    'Geschicklichkeit',
+    'Intelligenz',
+    'Zurückhaltung',
+    'Loyalität',
   ],
   datasets: [{
-    label: 'My First Dataset',
-    data: [6, 5, 9, 8, 5],
+    label: 'Paladin',
+    data: [5, 7, 8, 1, 10],
     fill: true,
     backgroundColor: 'rgba(255, 99, 132, 0.2)',
     borderColor: 'rgb(255, 99, 132)',
@@ -21,17 +22,7 @@ const data = {
     pointBorderColor: '#fff',
     pointHoverBackgroundColor: '#fff',
     pointHoverBorderColor: 'rgb(255, 99, 132)'
-  }, {
-    label: 'My Second Dataset',
-    data: [2, 4, 4, 1, 9],
-    fill: true,
-    backgroundColor: 'rgba(54, 162, 235, 0.2)',
-    borderColor: 'rgb(54, 162, 235)',
-    pointBackgroundColor: 'rgb(54, 162, 235)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgb(54, 162, 235)'
-  }]
+  },]
 };
 
 const config = {
@@ -47,6 +38,9 @@ const config = {
 };
 
 $(function () {
+    let svg = new Image();
+    svg.src = Paladin;
+    $(".character-avatar").append(svg);
     Chart.register(RadarController, LineElement, PointElement, RadialLinearScale, Title);
     new Chart($(".character-mainStats--radar-chart").get(), config);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,11 @@ module.exports = {
           // Compiles Sass to CSS
           "sass-loader"
         ]
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ]
   },
   watch: true,
